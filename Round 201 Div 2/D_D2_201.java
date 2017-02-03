3f5c5f0895b6c8d9da33c170ca60b855800a5115
atiqorin <atiqorin@gmail.com>
2017-02-03 14:39:03 -0600

    :bulb: Round 201 Div 2 Problem D Java

--- Round 201 Div 2/D_D2_201.java
+++ Round 201 Div 2/D_D2_201.java
@@ -2,91 +2,123 @@ import java.io.*;
 import java.util.*;
 
 public class D_D2_201 {
-    public static void main(String [] args) throws IOException {
+    public static int[] automata;
+    static int s1len, s2len, M;
+    static String s1, s2, virus;
+    static int [][][] dp; 
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(System.in);
-        String s1 = in.nextLine();
-        String s2 = in.nextLine();
-        String virus = in.nextLine();
-        String ans = lcs(s1, s2, virus);
-        System.out.println(ans.length() == 0 ? "0": ans);
-   }
+        s1 = in.nextLine();
+        s2 = in.nextLine();
+        virus = in.nextLine();
+        s1len = s1.length();
+        s2len = s2.length();
+        M = virus.length();
+        sb = new StringBuilder();
+        dp = new int[s1len+1][s2len+1][M+1];
+        for(int[][] twoD : dp){
+            for(int[] oneD : twoD){
+                Arrays.fill(oneD, -1);
+            }
+        }
+        automata = buildAutomata(virus);
 
-   public static int[] buildAutomata(String pat) {
-       int M = pat.length();
-       int atmataIndx = 0;
-       int patIndx = 1;
-       int[] automata = new int[M];
-       while(patIndx < M) {
-           if(pat.charAt(atmataIndx) == pat.charAt(patIndx)){
-               atmataIndx++;
-               automata[patIndx] = atmataIndx;
-               patIndx++; 
-           } else {
-               if(atmataIndx != 0) {
-                   atmataIndx = automata[atmataIndx - 1];
-               } else {
-                   automata[patIndx] = 0;
-                   patIndx++;
-               }
-           }
-       }
-       return automata;
-   }
+        int l = lcsdp(0,0,0);
+        if(l==0) {
+            System.out.println("0");
+        } else {
+            print(0,0,0);
+            System.out.println(sb);
+        }
+    }
 
-   public static boolean KMP(String str, String pat) {
-       int[] automata = buildAutomata(pat);
-       int M = pat.length();
-       int N = str.length();
+    public static int[] buildAutomata(String pat) {
+        int M = pat.length();
+        int atmataIndx = 0;
+        int patIndx = 1;
+        int[] automata = new int[M];
+        while (patIndx < M) {
+            if (pat.charAt(atmataIndx) == pat.charAt(patIndx)) {
+                atmataIndx++;
+                automata[patIndx] = atmataIndx;
+                patIndx++;
+            } else {
+                if (atmataIndx != 0) {
+                    atmataIndx = automata[atmataIndx - 1];
+                } else {
+                    automata[patIndx] = 0;
+                    patIndx++;
+                }
+            }
+        }
+        return automata;
+    }
 
-       int atmataIndx = 0;
-       int i = 0;
-       while(i < N) {
-           if(str.charAt(i) == pat.charAt(atmataIndx)){
-               i++;
-               atmataIndx++;
-               if(atmataIndx == M) {
-                   return true;
-               }
-           } else if(i < N) {
-               if(atmataIndx != 0) {
-                   atmataIndx = automata[atmataIndx - 1];
-               } else {
-                   i++;
-               }
-           }
-       }
-       return false;
-   }
-   public static String lcs(String a, String b, String virus) {
-       int [][] LCS = new int[a.length() + 1][b.length() + 1];
+    public static int lcsdp(int i, int j, int v) {
+        int vBefore = v;
+        if(i == s1len || j == s2len)
+            return dp[i][j][v] = 0;
+        if(dp[i][j][v] != -1)
+            return dp[i][j][v];
 
-       for (int i = 0; i < a.length(); i++) {
-           for (int j = 0; j < b.length(); j++) {
-               if(a.charAt(i) == b.charAt(j)){
-                   LCS[i + 1][j + 1] = LCS[i][j] + 1;
-               } else {
-                   LCS[i + 1][j + 1] = Math.max(LCS[i][j + 1], LCS[i + 1][j]);
-               }
-           }
-       }
+        int ret = 0;
+        if(s1.charAt(i) == s2.charAt(j)){
+            if(s1.charAt(i) == virus.charAt(v)){
+                if(v+1 < M) {
+                    ret = lcsdp(i+1, j+1, v+1) + 1;
+                }
+            }
+            else {
+                while(v != 0 && s1.charAt(i) != virus.charAt(v)){
+                    v = automata[v - 1];
+                }
+                if(s1.charAt(i) == virus.charAt(v)) {
+                    ret = lcsdp(i+1, j+1, v+1) + 1;
+                } 
+                else {
+                    ret = lcsdp(i+1, j+1, v) + 1;   
+                }
+            }
+        }
+        ret = Math.max(ret, lcsdp(i+1, j, vBefore));
+        ret = Math.max(ret, lcsdp(i, j+1, vBefore));
+        return dp[i][j][vBefore] = ret;
+    }
 
-       StringBuilder sb = new StringBuilder();
+    public static void print(int i, int j, int v) {
+        int vBefore = v;
+        if(i == s1len || j == s2len) return;
 
-       for(int i = a.length(), j = b.length(); i != 0 && j != 0; ) {
-           if(a.charAt(i - 1) == b.charAt(j - 1)) {
-               if(!KMP(sb.reverse().toString() + a.charAt(i - 1), virus)) {
-                    sb.reverse();
-                    sb.append(a.charAt(i - 1));
-               }
-               i--;
-               j--;
-           } else if(LCS[i][j] == LCS[i - 1][j]) {
-               i--;
-           } else if(LCS[i][j] == LCS[i][j - 1]) {
-               j--;
-           }
-       }
-
-       return sb.reverse().toString();
-   }
+        if(s1.charAt(i) == s2.charAt(j)) {
+            if(s1.charAt(i) == virus.charAt(v)){
+                if(v + 1 < M && dp[i][j][v] == dp[i+1][j+1][v+1] + 1) {
+                    sb.append(s1.charAt(i));
+                    print(i+1, j+1, v+1);
+                    return;
+                }
+            }
+            else {
+                while(v != 0 && s1.charAt(i) != virus.charAt(v)){
+                    v = automata[v-1];
+                }
+                if(s1.charAt(i) == virus.charAt(v) && dp[i][j][vBefore] == dp[i+1][j+1][v+1]+1){
+                    sb.append(s1.charAt(i));
+                    print(i+1, j+1, v+1);
+                    return;
+                } else {
+                    if(dp[i][j][vBefore] == dp[i+1][j+1][v] + 1) {
+                        sb.append(s1.charAt(i));
+                    }
+                    print(i+1, j+1,v);
+                    return;
+                }
+            }
+        }
+        if(dp[i][j][vBefore] == dp[i+1][j][vBefore]){
+            print(i+1,j,vBefore);
+            return;
+        }
+        print(i,j+1,vBefore);
+    }
 }
